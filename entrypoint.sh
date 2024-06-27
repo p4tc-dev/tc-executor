f5f57c69bb9f077df02d05efae2702942a040ff2
Pedro Tammela <pctammela@mojatatu.com>
2024-06-26 22:40:40 -0400

    docker: stop potential compute burglars
    Signed-off-by: Pedro Tammela <pctammela@mojatatu.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,9 @@
 
 if [ "$(cat /debug-mode)" = "true" ]; then
 	echo "=== >>> DEBUG KERNEL <<< ==="
-	./nipa/contest/remote/vmksft-p.py /tmp/tdc-debug.config
+	timeout 10m ./nipa/contest/remote/vmksft-p.py /tmp/tdc-debug.config
 else
-	./nipa/contest/remote/vmksft-p.py /tmp/tdc.config
+	timeout 10m ./nipa/contest/remote/vmksft-p.py /tmp/tdc.config
 fi
 
 chown -R hostuser:hostuser /storage 
